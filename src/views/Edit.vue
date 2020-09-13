8ef2082971e0fc7ce52aa4f456e18823289535aa
neneta0921 <info@neneta.net>
2020-09-13 16:21:30 +0900

    fixed bug

--- src/views/Edit.vue
+++ src/views/Edit.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="editor">
         <h2>Edit Memo</h2>
-        <textarea name="memo" v-model="memo.body"></textarea>
+        <textarea name="memo" v-model="memoBody"></textarea>
         <div>
             <button class="button" @click="update">保存</button>
         </div>
@@ -11,17 +11,21 @@
 <script>
 export default {
     name: 'edit',
-    computed: {
-        memo: function() {
-            let id = this.$route.params["id"];
-            return this.$store.state.memos.find(memo => memo.id == id);
-        },
+    data: function() {
+        return {
+            memoBody: ''
+        }
+    },
+    mounted: function() {
+        let memo_id = this.$route.params["id"];
+        let edit_memo = this.$store.state.memos.find(memo => memo.id == memo_id);
+        this.memoBody = edit_memo.body;
     },
     methods: {
         update: function() {
             this.$store.commit("updated", {
-                id: this.memo.id,
-                body: this.memo.body
+                id: this.$route.params["id"],
+                body: this.memoBody
             });
             this.$router.push('/');
         }
